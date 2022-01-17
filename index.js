22c947556536e8e38326efe27976a3aff3304b33
Ramzan <ramzan4ik95@mail.ru>
2022-01-17 16:15:16 +0300

    new server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const path = require('path')
+require("dotenv").config();
+
+const app = express()
+const port = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+app.use('/static', express.static(path.resolve(__dirname, 'static')));
+app.use(require('./routes'))
+
+
+const connected = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_SERVER)
+    console.log('Вы успешно подключились к MongoDB!');
+
+    app.listen(port, () => {
+      console.log('Server has been started!');
+    })
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+connected()
