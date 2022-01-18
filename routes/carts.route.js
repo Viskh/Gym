ccbeb59981b136d51f1103f098649039802df92f
Bilal <obkh1309@mail.ru>
2022-01-18 10:54:43 +0300

    Исправление ошибок с мидлом

--- routes/carts.route.js
+++ routes/carts.route.js
@@ -3,7 +3,7 @@ const {cartsController} = require('../controllers/carts.controller')
 const {authMiddleWare} = require('../middleweares/auth.middleware')
 const router = Router()
 
-router.get('/users/carts/', authMiddleWare, cartsController.getCartById);
-router.post('/users/carts/', authMiddleWare, cartsController.cartToken);
-router.patch('/users/carts/:id', authMiddleWare, cartsController.updateCart)
-router.delete('/users/:id', authMiddleWare, cartsController.deleteCart)
+router.get('/carts',authMiddleWare, cartsController.getCartById);
+router.post('/carts',authMiddleWare, cartsController.cartToken);
+router.patch('/carts/:id',authMiddleWare, cartsController.updateCart)
+router.delete('/carts/:id',authMiddleWare, cartsController.deleteCart)
