daed0b229e88d5681dcf5489ec5e5ab5ae63412e
Bilal <obkh1309@mail.ru>
2022-01-18 11:08:07 +0300

    изменение на карт

--- routes/carts.route.js
+++ routes/carts.route.js
@@ -1,9 +1,11 @@
 const {Router} = require('express')
 const {cartsController} = require('../controllers/carts.controller')
-const {authMiddleWare} = require('../middleweares/auth.middleware')
+const authMiddleWare = require('../middleweares/auth.middleware')
 const router = Router()
 
 router.get('/carts',authMiddleWare, cartsController.getCartById);
 router.post('/carts',authMiddleWare, cartsController.cartToken);
 router.patch('/carts/:id',authMiddleWare, cartsController.updateCart)
 router.delete('/carts/:id',authMiddleWare, cartsController.deleteCart)
+
+module.exports = router;
