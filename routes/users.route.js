2ff7717b75abe042b986201d5ed0c4301969991d
Ramzan <ramzan4ik95@mail.ru>
2022-01-18 10:14:07 +0300

    install bcrypt jwt

--- routes/users.route.js
+++ routes/users.route.js
@@ -4,7 +4,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/users", usersController.getAllUsers);
-router.post("/users", usersController.registerUser);
-router.post("/login", usersController.login);
+router.post("/users/register", usersController.registerUser);
+router.post("/users/login", usersController.login);
 
 module.exports = router;
