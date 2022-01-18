953faf025d2b65f0378437535f793321ccbc84c9
Ramzan <ramzan4ik95@mail.ru>
2022-01-18 15:23:54 +0300

    изменил модель user

--- models/Cart.model.js
+++ models/Cart.model.js
@@ -1,20 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const cartSchema = mongoose.Schema({
-  user: {
-    ref: 'User',
-    type: mongoose.SchemaTypes.ObjectId
+const cartSchema = mongoose.Schema(
+  {
+    user: {
+      ref: "User",
+      type: mongoose.SchemaTypes.ObjectId,
+    },
+    subscription: {
+      ref: "Subscription",
+      type: mongoose.SchemaTypes.ObjectId,
+    },
+    trainer: {
+      ref: "Trainer",
+      type: mongoose.SchemaTypes.ObjectId,
+    },
   },
-  subscription: {
-    ref: 'Subscription',
-    type: mongoose.SchemaTypes.ObjectId
-  },
-  trainer: {
-    ref: 'Trainer',
-    type: mongoose.SchemaTypes.ObjectId
-  }
-});
+  { timestamps: true }
+);
 
-const Cart = mongoose.model('Cart', cartSchema);
+const Cart = mongoose.model("Cart", cartSchema);
 
 module.exports = Cart;
--- models/Trainer.model.js
+++ models/Trainer.model.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const trainerSchema = mongoose.Schema({
+const trainerSchema = mongoose.Schema(
+  {
     name: String,
     description: String,
     img: {
-        type: String,
-        default: null
+      type: String,
+      default: null,
     },
     rating: Number,
     coefficient: Number,
-})
+  },
+  { timestamps: true }
+);
 
-const Trainer = mongoose.model('Trainer', trainerSchema);
+const Trainer = mongoose.model("Trainer", trainerSchema);
 
-module.exports = Trainer
+module.exports = Trainer;
