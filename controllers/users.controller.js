c1d4d9e36f228a1cab2d297107f6d03f3795bc42
Ramzan <ramzan4ik95@mail.ru>
2022-01-18 11:09:24 +0300

    controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -4,45 +4,60 @@ const jwt = require("jsonwebtoken");
 
 module.exports.usersController = {
   getAllUsers: async (req, res) => {
-    const users = await User.find();
-    res.json(users);
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (error) {
+      res.json(error);
+    }
   },
 
   registerUser: async (req, res) => {
-    const { login, password } = req.body;
+    try {
+      const { login, password } = req.body;
 
-    const hash = await bcrypt.hash(password, Number(process.env.BCRYPT_ROUNDS));
+      const hash = await bcrypt.hash(
+        password,
+        Number(process.env.BCRYPT_ROUNDS)
+      );
 
-    const user = await User.create({ login: login, password: hash });
+      const user = await User.create({ login: login, password: hash });
 
-    res.json(user);
+      res.json(user);
+    } catch (error) {
+      res.json(error);
+    }
   },
 
   login: async (req, res) => {
-    const { login, password } = req.body;
+    try {
+      const { login, password } = req.body;
 
-    const condidate = await User.findOne({ login });
+      const condidate = await User.findOne({ login });
 
-    if (!condidate) {
-      return res.status(401).json("неверный логин или пароль");
+      if (!condidate) {
+        return res.status(401).json("неверный логин или пароль");
+      }
+
+      const valid = await bcrypt.compare(password, condidate.password);
+      if (!valid) {
+        return res.status(401).json("неверный логин или пароль");
+      }
+
+      const payload = {
+        id: condidate._id,
+        login: condidate.login,
+      };
+
+      const token = await jwt.sign(payload, process.env.SECRET_JWT_KEY, {
+        expiresIn: "21d",
+      });
+
+      res.json({
+        token,
+      });
+    } catch (error) {
+      res.json(error);
     }
-
-    const valid = await bcrypt.compare(password, condidate.password);
-    if (!valid) {
-      return res.status(401).json("неверный логин или пароль");
-    }
-
-    const payload = {
-      id: condidate._id,
-      login: condidate.login,
-    };
-
-    const token = await jwt.sign(payload, process.env.SECRET_JWT_KEY, {
-      expiresIn: "21d",
-    });
-
-    res.json({
-      token,
-    });
   },
 };
