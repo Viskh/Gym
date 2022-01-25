e4e574c3a61ab237abb35e1ad9738864d667f7ba
Iznaur18 <gimer8@mail.ru>
2022-01-25 16:09:41 +0300

    upload photo

--- routes/trainers.route.js
+++ routes/trainers.route.js
@@ -1,11 +1,13 @@
 const {Router} = require('express')
 const {trainersController} = require('../controllers/trainers.controller')
 const authMiddlewares = require('../middlewares/auth.middlewares')
+const addFileMiddleware = require("../middlewares/addFile.middleware");
 const router = Router()
 
 router.post('/admin/trainers', trainersController.createTrainer)
 router.get('/users/trainers', trainersController.getAllTrainers)
 router.patch('/admin/trainers/:id', trainersController.updateTrainer)
 router.delete('/admin/trainers/:id', trainersController.deleteTrainer)
+router.patch('/admin/trainers/image/:id', addFileMiddleware.single('img'), trainersController.updateImg);
 
 module.exports = router
