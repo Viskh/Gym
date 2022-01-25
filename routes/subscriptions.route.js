8f130c3bb1ff1aab6ce06c41bc31cd746ea0bfcb
Iznaur18 <gimer8@mail.ru>
2022-01-25 14:16:44 +0300

    мидлвер убрал

--- routes/subscriptions.route.js
+++ routes/subscriptions.route.js
@@ -5,7 +5,7 @@ const authMiddlewares = require('../middlewares/auth.middlewares')
 const router = Router()
 
 router.get('/users/subscriptions', subscriptionsController.getAllSubscriptions)
-router.post('/admin/subscriptions', authMiddlewares, subscriptionsController.creareSubscription)
+router.post('/admin/subscriptions', subscriptionsController.creareSubscription)
 router.delete('/admin/subscriptions/:id', authMiddlewares, subscriptionsController.deleteSubscription)
 
 module.exports = router
