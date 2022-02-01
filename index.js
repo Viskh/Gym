5fd874e20c9ff1736843678924c2dbef739eb2a8
Ramzan <ramzan4ik95@mail.ru>
2022-02-01 14:06:06 +0300

    исправлены ошибки heroku

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-app.use("/static", express.static(path.resolve(__dirname, "client", "static")));
+app.use(express.static(path.resolve(__dirname, "client", "static")));
 app.use(require("./routes"));
 
 app.get("*", (req, res) => {
